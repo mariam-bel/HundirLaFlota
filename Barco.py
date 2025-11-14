da22eeb3f133db0041bd43312a1a23d7bd3098fd
mariam-bel <merimarabel@gmail.com>
2025-11-14 20:24:13 +0100

    Actualizada la clase Barco con las siguientes funcionalidades: genera las coordenadas(asegurándose de no repetirse), recoge el tamaño del barco y su nombre en el constructor

--- Barco.py
+++ Barco.py
@@ -1,15 +1,23 @@
+import random
+
+
 class Barco:
     barcos = ["lancha","corbeta","fragata","acorazado"]
+    coords = []
     def __init__(self, nombre):
         if nombre in self.barcos:
             self.__nombre = nombre.lower()
         else:
             raise NameError("Introduzca un barco correcto.")
+        self.__size = self.__getSize
+        self.__coords = self.__generateCoords(self.__size, self.coords)
+
 
     def __getNombre(self):
         return self.__nombre
 
-    def getSize(self):
+    @property
+    def __getSize(self):
         size = 0
         if(self.__nombre == "lancha"):
             size = 1
@@ -20,3 +28,29 @@ class Barco:
         if(self.__nombre == "acorazado"):
             size = 4
         return size
+
+    @staticmethod
+    def __generateCoords(size, coords):
+        coordenadasBarco = []
+        for i in range(size):
+            coord = [random.randint(0,10),random.randint(0,10)]
+            while coord in coords:
+                coord = [random.randint(0, 10), random.randint(0, 10)]
+            coordenadasBarco.append(coord)
+            coords.append(coord)
+        return coordenadasBarco
+
+    def __str__(self):
+        return f"Barco: {self.__nombre} | Tamaño: {self.__getSize} | Coordenadas: {self.__coords}"
+
+
+b  = Barco("lancha")
+b2 = Barco("corbeta")
+b22 = Barco("corbeta")
+b3 = Barco("fragata")
+b4 = Barco("acorazado")
+print(b)
+print(b2)
+print(b22)
+print(b3)
+print(b4)
--- Mapa.py
+++ Mapa.py
@@ -1,3 +1,23 @@
-class Mapa:
-    def __init__(self, nombre):
-        self.__nombre = nombre
+
+#Aleatorio
+mapa = []
+def generarMapa(mapa):
+    for i in range(10):
+        mapa.append([])
+        for j in range(10):
+            mapa[i].append("~")
+generarMapa(mapa)
+
+def printMapa(mapa):
+    x = 1
+    print("    A    B    C    D    E    F    G    H    J    K")
+    for fila in mapa:
+        if x != 10:
+            print(f"{x} {fila}")
+        else:
+            print(f"{x}{fila}")
+        x += 1
+
+
+printMapa(mapa)
+
